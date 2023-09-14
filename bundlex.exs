0c5adffea3caca146c0b718b4627cb206d17017e
varsill <lukasz.kita0@gmail.com>
2023-09-14 13:55:39 +0000

    Implement reviewers suggestions

--- bundlex.exs
+++ bundlex.exs
@@ -1,20 +1,25 @@
 defmodule Membrane.Agora.BundlexProject do
   use Bundlex.Project
 
-  defmodule TargetGetter do
-    def get_target() do
-      [architecture, vendor, os | _rest] =
-        :erlang.system_info(:system_architecture) |> List.to_string() |> String.split("-")
+  def get_target() do
+    [architecture, vendor, os | _rest] =
+      :erlang.system_info(:system_architecture) |> List.to_string() |> String.split("-")
 
-      %{
-        architecture: architecture,
-        vendor: vendor,
-        os: os
-      }
-    end
+    %{
+      architecture: architecture,
+      vendor: vendor,
+      os: os
+    }
   end
 
   def project do
+    case TargetGetter.get_target() do
+      %{os: "linux"} ->
+        System.shell("./install.sh")
+      other_target ->
+        IO.warn("Agora's Server Gateway SDK is unavailable for this target: #{inspect(other_target)}")
+    end
+
     [
       natives: natives(Bundlex.platform())
     ]
@@ -33,16 +38,6 @@ defmodule Membrane.Agora.BundlexProject do
         language: :cpp
       ]
     ]
-  end
-
-
-
-  case TargetGetter.get_target() do
-    %{os: "linux"} ->
-      System.shell("chmod a+x install.sh")
-      System.shell("./install.sh")
-    other_target ->
-      IO.warn("Agora's Server Gateway SDK is unavailable for this target: #{inspect(other_target)}")
   end
 
 end
