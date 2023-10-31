b7e9dffb5ff4ac860e3a244e68a6347437de83ca
varsill <lukasz.kita0@gmail.com>
2023-10-31 12:27:17 +0100

    Bump to v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Agora.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @github_url "https://github.com/membraneframework/membrane_agora_plugin"
 
   def project do
