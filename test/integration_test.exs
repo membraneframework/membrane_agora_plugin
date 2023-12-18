ab08324d73a29620ee61af4e8085a0b081bd1ba9
varsill <lukasz.kita0@gmail.com>
2023-12-18 14:28:57 +0000

    Improve assertions in integration test

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -3,11 +3,8 @@ defmodule Membrane.Agora.IntegrationTest do
 
   import Membrane.Testing.Assertions
 
-  defmodule PipelineWithAgoraSource do
+  defmodule ReceiverPipeline do
     use Membrane.Pipeline
-
-    @output_video_path "test_output_video.h264"
-    @output_audio_path "test_output_audio.pcm"
 
     @channel_name System.get_env("AGORA_CHANNEL_NAME", "")
     @token System.get_env("AGORA_TOKEN", "")
@@ -15,7 +12,7 @@ defmodule Membrane.Agora.IntegrationTest do
     @user_id System.get_env("AGORA_USER_ID", "0")
 
     @impl true
-    def handle_init(_ctx, _options) do
+    def handle_init(_ctx, opts) do
       spec = [
         child(:source, %Membrane.Agora.Source{
           channel_name: @channel_name,
@@ -24,21 +21,18 @@ defmodule Membrane.Agora.IntegrationTest do
           user_id: @user_id
         })
         |> via_out(:video)
-        |> child(:video_sink, %Membrane.File.Sink{location: @output_video_path}),
+        |> child(:video_sink, %Membrane.File.Sink{location: opts[:video]}),
         get_child(:source)
         |> via_out(:audio)
-        |> child(:audio_sink, %Membrane.File.Sink{location: @output_audio_path})
+        |> child(:audio_sink, %Membrane.File.Sink{location: opts[:audio]})
       ]
 
       {[spec: spec], %{}}
     end
   end
 
-  defmodule PipelineWithAgoraSink do
+  defmodule SenderPipeline do
     use Membrane.Pipeline
-
-    @input_video_path "test/fixtures/in_video.h264"
-    @input_audio_path "test/fixtures/in_audio.aac"
 
     @channel_name System.get_env("AGORA_CHANNEL_NAME", "")
     @token System.get_env("AGORA_TOKEN", "")
@@ -46,12 +40,12 @@ defmodule Membrane.Agora.IntegrationTest do
     @user_id System.get_env("AGORA_USER_ID", "0")
 
     @impl true
-    def handle_init(_ctx, _options) do
+    def handle_init(_ctx, opts) do
       spec =
         [
-          child(%Membrane.File.Source{location: @input_video_path})
+          child(%Membrane.File.Source{location: opts[:video]})
           |> child(%Membrane.H264.Parser{
-            generate_best_effort_timestamps: %{framerate: {30, 1}}
+            generate_best_effort_timestamps: %{framerate: {12, 1}}
           })
           |> child(Membrane.Realtimer)
           |> via_in(:video)
@@ -61,7 +55,7 @@ defmodule Membrane.Agora.IntegrationTest do
             app_id: @app_id,
             user_id: @user_id
           }),
-          child(%Membrane.File.Source{location: @input_audio_path})
+          child(%Membrane.File.Source{location: opts[:audio]})
           |> child(Membrane.AAC.Parser)
           |> child(Membrane.Realtimer)
           |> via_in(:audio)
@@ -72,16 +66,68 @@ defmodule Membrane.Agora.IntegrationTest do
     end
   end
 
-  test "if the data is sent to Agora properly" do
-    {:ok, _supervisor, sink_pipeline} = Membrane.Pipeline.start_link(PipelineWithAgoraSink)
+  @tag :tmp_dir
+  test "if the data is sent to Agora properly", %{tmp_dir: dir} do
+    require Membrane.Pad, as: Pad
 
-    {:ok, _supervisor, source_pipeline} =
-      Membrane.Testing.Pipeline.start_link(module: PipelineWithAgoraSource)
+    input_video = "test/fixtures/in_video.h264"
+    output_video = "#{dir}/video.h264"
+    reference_video = input_video
 
-    assert_start_of_stream(source_pipeline, :video_sink, :input, 10_000)
-    Process.sleep(5_000)
-    Membrane.Pipeline.terminate(sink_pipeline)
-    assert_end_of_stream(source_pipeline, :video_sink, :input, 10_000)
-    Membrane.Pipeline.terminate(source_pipeline)
+    input_audio = "test/fixtures/in_audio.aac"
+    output_audio = "#{dir}/audio.pcm"
+    reference_audio = "test/fixtures/in_audio.pcm"
+
+    {:ok, _supervisor, sender_pipeline} =
+      Membrane.Testing.Pipeline.start_link(
+        module: SenderPipeline,
+        custom_args: [audio: input_audio, video: input_video]
+      )
+
+    {:ok, _supervisor, receiver_pipelineline} =
+      Membrane.Testing.Pipeline.start_link(
+        module: ReceiverPipeline,
+        custom_args: [audio: output_audio, video: output_video]
+      )
+
+    assert_start_of_stream(receiver_pipelineline, :video_sink, :input, 10_000)
+    assert_start_of_stream(receiver_pipelineline, :audio_sink)
+
+    assert_end_of_stream(sender_pipeline, :sink, Pad.ref(:video, _), 30_000)
+    assert_end_of_stream(sender_pipeline, :sink, Pad.ref(:audio, _), 30_000)
+
+    Membrane.Pipeline.terminate(sender_pipeline)
+
+    assert_end_of_stream(receiver_pipelineline, :video_sink, :input, 10_000)
+    assert_end_of_stream(receiver_pipelineline, :audio_sink)
+
+    Membrane.Pipeline.terminate(receiver_pipelineline)
+
+    assert abs(
+             (get_h264_frames(reference_video)
+              |> length()) -
+               (get_h264_frames(output_video)
+                |> length())
+           ) < 5
+
+    assert abs(File.stat!(reference_audio).size - File.stat!(output_audio).size) <
+             100_000
+  end
+
+  defp get_h264_frames(path) do
+    alias Membrane.H264.Parser.{NALuSplitter, NALuParser, AUSplitter}
+    bytestream = File.read!(path)
+
+    nalu_splitter = NALuSplitter.new()
+    nalu_parser = NALuParser.new()
+    au_splitter = AUSplitter.new()
+
+    {nalu_payloads, _nalu_splitter} = NALuSplitter.split(bytestream, true, nalu_splitter)
+
+    {nalus, _nalu_parser} = Enum.map_reduce(nalu_payloads, nalu_parser, &NALuParser.parse(&1, &2))
+
+    {aus, _au_splitter} = AUSplitter.split(nalus, true, au_splitter)
+
+    aus
   end
 end
