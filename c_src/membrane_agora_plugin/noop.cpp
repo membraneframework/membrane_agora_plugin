5e46d8fb86f80e5e738ba9871dbb61646c7863a6
Nick Dichev <nick.dichev@fireworkhq.com>
2024-03-29 08:39:36 -0700

    Add a comment about noop.cpp

--- c_src/membrane_agora_plugin/noop.cpp
+++ c_src/membrane_agora_plugin/noop.cpp
@@ -1,3 +1,8 @@
+/*
+ This is a small noop file so bundlex has something to compile on non-linux machines.
+ The code below is the smallest nif loading code (from the erlang docs) since typically
+ unifex will add it, however, we don't want to include unifex here.
+ */
 #include <erl_nif.h>
 
 static ERL_NIF_TERM hello(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
