4e2f97eede9f9b0cc0a89a55ce9d8302717ad576
Łukasz Kita <lukasz.kita0@gmail.com>
2024-11-29 10:57:30 +0100

    Measure time of Agora SDK initialization and warn if it is longer than expected

--- lib/agora/agora_sink.ex
+++ lib/agora/agora_sink.ex
@@ -5,6 +5,7 @@ defmodule Membrane.Agora.Sink do
   """
   use Membrane.Sink
 
+  require Membrane.Logger
   require Membrane.Pad, as: Pad
 
   alias Membrane.Agora.Sink.Native
@@ -63,8 +64,19 @@ defmodule Membrane.Agora.Sink do
   @impl true
   def handle_playing(_ctx, state) do
     {:ok, native_state} =
-      try do
+      try
+        start_time = Membrane.Time.os_time()
         Native.create(state.app_id, state.token, state.channel_name, state.user_id)
+        duration = Membrane.Time.os_time()-start_time
+        duration_ms = Membrane.Time.as_milliseconds(duration, :round)
+        Membrane.Logger.info("Agora SDK initialization took: #{duration_ms} ms")
+        if duration_ms > 500 do
+          Membrane.Logger.warning("""
+          Agora SDK initialization took #{duration_ms} ms which is longer than expected.
+          The initial demand made by this sink might be delayed.
+          """)
+        end
+
       rescue
         _e in UndefinedFunctionError ->
           reraise(
